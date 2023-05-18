a56c1c3704af76529348fe2b452963c2be8cb020
Anish Wagle <Anish.wagle192@gmail.com>
2023-05-18 06:31:21 +0545

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { FirstComponentComponent } from './first-component/first-component.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button'; 
 @NgModule({
   declarations: [
     AppComponent,
@@ -15,6 +17,10 @@ import { FirstComponentComponent } from './first-component/first-component.compo
     BrowserModule,
     AppRoutingModule,
     FormsModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/first-component/first-component.component.ts
+++ src/app/first-component/first-component.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-first-component',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./first-component.component.css']
 })
 export class FirstComponentComponent {
+userForm = new FormGroup({
+  name:new FormControl(),
+  age:new FormControl("18")
+})
 
+
+onSave(){
+  if(!this.userForm.get("name")?.value){
+    this.userForm.patchValue({name:"Hari"})
+  }
+  console.log(`Name:${this.userForm.get("name")?.value},Age:${this.userForm.get("age")?.value}`);
+  
+}
 }
